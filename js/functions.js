f4e8ce53f1e0f00b8c28b5e1ea633c73cf9ce666
Vincent CARDONA <vincentcardona4@mail.com>
2022-01-31 16:21:48 +0100

    3eme commit

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,9 @@
  * FONCTIONS DE VÉRIFICATIONS D'UN FORMULAIRE
  */
 
-// Vérifie que la donnée reçue ne soit pas vide
+/**
+ * Vérifie que la donnée reçue ne soit pas vide
+ */
 let notEmpty = function(value) {
 
     // Si la longueur de la châine est à zéro, c'est que celle-ci
@@ -14,4 +16,22 @@ let notEmpty = function(value) {
     return true;
 
     // return value.length === 0;
+}
+
+
+/**
+ * Vérifie que la donnée contient un certains nombre de caractères
+ */
+let isLength = function(value, min) {
+    // Si la chaîne fait 5 caractères ou plus, on retourne "true"
+    return value.length >= Number(min);
+}
+
+
+/**
+ * Vérifie que la donnée soit une adresse email valide
+ */
+let isEmail = function(value) {
+    const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,10}$/;
+    return pattern.test(value);
 }
--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
  * VÉRIFICATION D'UN FORMULAIRE
  */
 
+
+/**
+ * VÉRIFICATION DU NOM
+ */
+
 // Sélectionne le champ texte du nom
 let lastname = document.querySelector('#lastname');
 
@@ -21,6 +26,93 @@ lastname.addEventListener('input', function() {
 
     // Applique une bordure verte si le champ n'est pas vide
     // Equivalent : if (response === true) { ... }
+    if (response) {
+        this.style.border = '2px solid green';
+        error.style.display = 'none';
+    }
+});
+
+
+/**
+ * VÉRIFICATION DU PRÉNOM
+ */
+
+// Sélectionne le champ texte du prénom
+let firstname = document.querySelector('#firstname');
+
+// Applique un écouteur d'évènement sur les changements effectués dans le champ
+// https://developer.mozilla.org/fr/docs/Web/API/HTMLElement/input_event
+firstname.addEventListener('input', function() {
+    
+    let response = notEmpty(this.value);
+    let error = document.querySelector('#firstnameError');
+
+    this.style.border = '2px solid red';
+    error.style.display = 'block';
+
+    if (response) {
+        this.style.border = '2px solid green';
+        error.style.display = 'none';
+    }
+});
+
+
+/**
+ * VÉRIFICATION DU PSEUDO
+ */
+
+// Sélectionne le champ texte du prénom
+let pseudo = document.querySelector('#pseudo');
+pseudo.addEventListener('input', function() {
+
+    // Vérfifie la longueur d'une chaîne
+    let response = isLength(this.value, 5);
+    let error = document.querySelector('#pseudoError');
+
+    this.style.border = '2px solid red';
+    error.style.display = 'block';
+
+    if (response) {
+        this.style.border = '2px solid green';
+        error.style.display = 'none';
+    }
+});
+
+
+/**
+ * VÉRIFICATION DE L'ADRESSE EMAIL
+ */
+let email = document.querySelector('#email');
+email.addEventListener('input', function() {
+
+    let response = isEmail(this.value);
+    let error = document.querySelector('#emailError');
+
+    this.style.border = '2px solid red';
+    error.style.display = 'block';
+
+    if(response) {
+        this.style.border = '2px solid green';
+        error.style.display = 'none';
+    }
+});
+
+
+/**
+ * VÉRIFICATION DU MOT DE PASSE
+ */
+
+// Sélectionne le champ texte du prénom
+let password = document.querySelector('#password');
+password.addEventListener('input', function() {
+
+    // Vérifie la longueur d'une chaîne
+    let response = isLength(this.value, 6);
+    let error = document.querySelector('#passwordError');
+
+    this.style.border = '2px solid red';
+    error.style.display = 'block';
+
     if (response) {
         this.style.border = '2px solid green';
         error.style.display = 'none';
